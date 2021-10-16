df019d489fbc63abf04d46047d6470584e33d135
byung1211 <byung1211@edu.sait.ca>
2021-10-16 17:06:46 -0600

    Added the menu closeing icon and functions

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@
 * Date: 2021.10.16
 */
 
+'use strict';
 
 // 1. Created an EventHandler for the Hamburger menu.
- function showMenu() {
-  const navbar = document.getElementById("menu");
-  navbar.classList.toggle("show");
+ const showMenu = function() {
+  document.querySelector("#menu").classList.toggle("show");
+  document.querySelector("#menu-open-icon").classList.toggle("hide");
+  document.querySelector("#menu-close-icon").classList.toggle("show");
 }
 
 // 2. Registered the EventHandler by adding the EventListener.
