cfdff11a3ae27aa6529e207e4dac4d35134c93a0
byung1211 <byung1211@edu.sait.ca>
2021-10-16 16:36:21 -0600

    Improved code and added comments

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@
 * Date: 2021.10.16
 */
 
+
+// 1. Created an EventHandler for the Hamburger menu.
  function showMenu() {
-  var navbar = document.getElementById("menu");
+  const navbar = document.getElementById("menu");
   navbar.classList.toggle("show");
 }
+
+// 2. Registered the EventHandler by adding the EventListener.
+document.querySelector("#menu-btn").addEventListener("click", showMenu);
